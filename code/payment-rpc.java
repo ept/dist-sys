157b9d66d1e0788778aee763cdfe23f5826e46fb
Martin Kleppmann <martin@kleppmann.com>
2020-09-10 22:04:12 +0100

    Material on RPC

---
+++ code/payment-rpc.java
@@ -0,0 +1,12 @@
+// Online shop handling customer's card details
+Card card = new Card();
+card.setCardNumber("1234 5678 8765 4321");
+card.setExpiryDate("10/2024");
+card.setCVC("123");
+
+Result result = paymentsService.processPayment(card,
+    3.99, Currency.GBP);
+
+if (result.isSuccess()) {
+    fulfilOrder();
+}
