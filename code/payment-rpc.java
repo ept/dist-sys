b9da25db93b92793a554cefad3d475d934a685a7
Martin Kleppmann <martin@kleppmann.com>
2022-11-02 09:36:40 +0000

    sterling -> euro

--- code/payment-rpc.java
+++ code/payment-rpc.java
@@ -5,7 +5,7 @@ card.setExpiryDate("10/2024");
 card.setCVC("123");
 
 Result result = paymentsService.processPayment(card,
-    3.99, Currency.GBP);
+    3.99, Currency.EUR);
 
 if (result.isSuccess()) {
     fulfilOrder();
